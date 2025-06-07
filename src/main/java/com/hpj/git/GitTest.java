06ca9e87d6e2ac3f782957ad9662174883ad452d
hpjchilin <839973867@qq.com>
2025-06-07 17:46:56 +0800

    push commit2

--- src/main/java/com/hpj/git/GitTest.java
+++ src/main/java/com/hpj/git/GitTest.java
@@ -4,5 +4,6 @@ public class GitTest {
     public static void main(String[] args) {
         System.out.println("hello,git4!");
         System.out.println("master test!");
+        System.out.println("push test!");
     }
 }
