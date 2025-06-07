27999162b383ad4986507af738411ab81f3d7886
hpjchilin <839973867@qq.com>
2025-06-07 17:32:56 +0800

    master commit2

--- src/main/java/com/hpj/git/GitTest.java
+++ src/main/java/com/hpj/git/GitTest.java
@@ -3,5 +3,6 @@ package com.hpj.git;
 public class GitTest {
     public static void main(String[] args) {
         System.out.println("hello,git4!");
+        System.out.println("master test!");
     }
 }
